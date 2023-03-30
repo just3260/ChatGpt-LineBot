35e952e064e428e9acec8f072cad1504f9f57518
Andrew Wang <just3260@gmail.com>
2023-03-31 01:00:55 +0800

    update

--- api/index.py
+++ api/index.py
@@ -36,10 +36,13 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
-    
+
     if event.message.type != "text":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="我目前只支援文字對話喔！"))
         return
-    
+
     if event.message.text == "啟動":
         working_status = True
         line_bot_api.reply_message(
@@ -47,13 +50,13 @@ def handle_message(event):
             TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
         return
 
-    if event.message.text == "安靜":
+    if event.message.text == "關閉":
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
         return
-    
+
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
